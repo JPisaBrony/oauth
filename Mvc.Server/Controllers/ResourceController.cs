3a6b5e15aa7896be7344d3dbb3148fc8fae8e4bc
JPisaBrony <jdpohl789@gmail.com>
2016-08-15 14:04:40 -0500

    added code

---
+++ Mvc.Server/Controllers/ResourceController.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mvc.Server.Controllers {
+    [Route("api")]
+    public class ResourceController : Controller {
+        //[Authorize, HttpGet, Route("message")]
+        [HttpGet, Route("message")]
+        public IActionResult GetMessage() {
+            var identity = User.Identity as ClaimsIdentity;
+            if (identity == null) {
+                return BadRequest();
+            }
+
+            return Content($"{identity.Name} has been successfully authenticated by the Resource Controller.");
+        }
+    }
+}
